c8410634ba5a3df14bf4f60e41244d6d190d1ea7
Pawan Tiwari <ramtiwari7081@gmail.com>
2026-01-07 03:17:18 +0530

    feat: initialize react frontend with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-2xl font-bold">
+        Health On Door 🚑
+      </h1>
+    </div>
+  );
+}
+
+export default App
